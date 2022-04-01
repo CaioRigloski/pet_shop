1a1561037733740b44592202eb8d4705ba7f2407
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2022-04-01 00:57:51 -0300

    flash messages configured

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const app = express()
 const hbs = require('express-handlebars')
 const bodyParser = require('body-parser')
 const Contact = require('./src/database/contact')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const flash = require('req-flash')
 
 
 app.engine('handlebars', hbs.engine({
@@ -21,23 +24,48 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 app.use(bodyParser.json())
 
+app.use(cookieParser())
+app.use(session({
+  secret: 'ubg1234hijb12hb',
+  cookie: { maxAge: 60000},
+  store: new session.MemoryStore,
+  saveUninitialized: true,
+  resave: 'true',
+}))
+
+app.use(flash(function(req, res, next) {
+  res.locals.message = req.flash('message')
+  return next()
+}))
+
+
 app.get('/', (req, res) => {
   res.render('home')
 })
 
 app.get('/contato', (req, res) => {
-  res.render('contact')
+  res.render('contact', { message: req.flash('message') })
 })
 
 app.post('/contato', (req, res) => {
-  Contact.create({
-    name: req.body.name,
-    pet_name: req.body.pet_name,
-    phone: req.body.phone,
-    email: req.body.email
-  }).then(
+  var err
+  try {
+    Contact.create({
+      name: req.body.name,
+      pet_name: req.body.pet_name,
+      phone: req.body.phone,
+      email: req.body.email
+    })
+  } catch (error) {
+    err = true 
+  } finally {
+    if(err = true) {
+      req.flash('message', 'Erro')
+    } else {
+      req.flash('message', 'Enviado com sucesso!')
+    }
     res.redirect('/contato')
-  )
+  }
 })
 
 app.listen(8080)

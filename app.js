9c4f35c89d023f1ce1252856792f8e805ea4f1d3
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2022-04-02 01:22:55 -0300

    form send redirect to the same url, mini form on footer added, displayed unless it's on /contato

--- app.js
+++ app.js
@@ -45,12 +45,17 @@ app.use(flash(function(req, res, next) {
 
 
 app.get('/', (req, res) => {
-  res.render('home', { info: info })
+  res.render('home', {
+    info: info,
+    message_success: req.flash('message_success'),
+    message_error: req.flash('message_error')
+  })
 })
 
 app.get('/contato', (req, res) => {
   res.render('contact', {
     info: info,
+    contact_page: true,
     message_success: req.flash('message_success'),
     message_error: req.flash('message_error')
   })
@@ -64,7 +69,7 @@ app.post('/contato', (req, res) => {
   email: req.body.email
   }).then(() => req.flash('message_success', 'Enviado com sucesso!'))
   .catch(() => req.flash('message_error', `Erro ao enviar, por gentileza entre em contato conosco através do telefone ${info.phone}, Whatsapp ${info.cellphone}, ou e-mail ${info.email}`))
-  .then(() => res.redirect('/contato'))
+  .then(() => res.redirect(req.get('referer')))
 })
 
 app.listen(8080)

118dcc85584777244f45ffce7080b09e5d1e840d
Caio Henrique Rigloski <caio.henrique.rigloski@hotmail.com>
2022-05-12 23:45:31 -0300

    concluded contacts are saved with boolean row, Ajax request works fine

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,15 +1,17 @@
+// Conlude the contact
 $('.contact .conclude-btn').on({
   click: function() {
     var contactBox = $(this).closest('.contact-box')
+    var contactId = contactBox.attr('contactId')
     var email = contactBox.find('.email').text()
     $.ajax({
-      type: "POST",
+      type: 'POST',
       url: '/admin/painel/excluir_contato',
-      dataType: "json",
-      data: { email: email }
-    }).done(function(data) {
+      dataType: 'json',
+      data: { id: contactId, email: email }
+    }).done((data) => {
       console.log(data)
-    }).fail(function(error) {
+    }).fail((error) => {
       console.log(error)
     }).then((response) => {
       if(response === 'success') {
@@ -22,8 +24,37 @@ $('.contact .conclude-btn').on({
   }
 })
 
+// Close error modal
 $('#error_modal #error_modal_close').on({
   click: function() {
     $('#error_modal').hide()
   }
 })
+
+// Page on load
+$(
+  () => {
+    var path = window.location.pathname
+    if(path.match('/admin/painel/')) {
+      $.ajax({
+        type: "GET",
+        url: '/admin/painel/api/contacts',
+      }).done(() => {
+        
+      }).fail((error) => {
+        console.log(error)
+      }).then((response) => {
+        response.forEach((contact, i) => {
+          if(contact.is_concluded === 1) {
+            $(`[contactId = ${contact.id}]`).attr('style', 'background-color: green !important;')
+          }
+        })
+      })
+    }
+  }
+)
+
+
+
+
+
